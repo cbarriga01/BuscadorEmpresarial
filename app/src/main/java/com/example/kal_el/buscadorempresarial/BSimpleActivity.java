fa3d062edbb72471c9bc57e30706c61cb6a2df2e
cbarriga01 <c.barriga01@ufromail.cl>
2016-06-10 00:15:22 -0400

    test 2

--- app/src/main/java/com/example/kal_el/buscadorempresarial/BSimpleActivity.java
+++ app/src/main/java/com/example/kal_el/buscadorempresarial/BSimpleActivity.java
@@ -1,11 +1,12 @@
 package com.example.kal_el.buscadorempresarial;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class BSimpleActivity extends AppCompatActivity {
 
-    String dirIp = "192.168.1.36:8080";
+    String dirIp = "192.168.2.36:8080";
     String NAMESPACE = "http://webservice";
     String URL = "http://"+dirIp+"/TallerJSP/services/WebServiceProveedor?wsdl";
     String SOAP_ACTION = "http://"+dirIp+"/TallerJSP/services/WebServiceProveedor";
@@ -16,4 +17,5 @@ public class BSimpleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bsimple);
     }
+
 }
